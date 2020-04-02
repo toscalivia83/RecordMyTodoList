dbd2bdc27f57800c2bcf6eee9d985e0750d4d61e
Marion Gourlaouen <marion.gourlaouen@treatwell.com>
2020-04-02 10:53:25 +0100

    test add update delete functionalities

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -5,8 +5,8 @@ import { mount, ReactWrapper } from "enzyme";
 import { Provider } from "react-redux";
 import { createStoreWithState, createDefaultStore } from "../../redux/store";
 import { TodoType } from "../../redux/types";
-import TodoList from "../TodoList/TodoList";
 import Todo from "../Todo/Todo";
+import App from "../../App";
 
 const validTodo = (todoId: number, name = "test", description = "description"): TodoType => ({
   id: todoId,
@@ -19,9 +19,9 @@ const validTodoList: TodoType[] = [validTodo(0), validTodo(1)];
 
 describe("<TodoList /> suite", () => {
   let wrapper: ReactWrapper;
-
+  
   const setupTest = (store: Store): void => {
-    wrapper = mount(<Provider store={store}> <TodoList/> </Provider>);
+    wrapper = mount(<Provider store={store}> <App/> </Provider>);
   };
 
   it("displays the todo list correctly", () => {
@@ -36,5 +36,38 @@ describe("<TodoList /> suite", () => {
     setupTest(store);
 
     expect(wrapper.find(Todo)).toHaveLength(2);
+  });
+
+
+  it("adds, updates and deletes the todos to the todolist correctly", () => {
+    const defaultStore = createDefaultStore();
+    const store = createStoreWithState({
+      ...defaultStore.getState(),
+      todos: {
+        ...defaultStore.getState().todos,
+        todoList: []
+      }
+    });
+    setupTest(store);
+
+    expect(wrapper.find(Todo)).toHaveLength(0);
+    wrapper.find("input[name='todoToAdd']").simulate("change", { target: { value: "test1" } });
+    wrapper.find("button[name='addTodo']").simulate("click");
+    expect(store.getState().todos.todoList.length).toEqual(1);
+    expect(store.getState().todos.todoList[0].description).toEqual("test1");
+
+    wrapper.find("input[name='todoToAdd']").simulate("change", { target: { value: "test2" } });
+    wrapper.find("button[name='addTodo']").simulate("click");
+    expect(store.getState().todos.todoList.length).toEqual(2);
+    expect(store.getState().todos.todoList[1].description).toEqual("test2");
+
+    wrapper.find("button[name='edit']").last().simulate("click");
+    wrapper.find("input[name='todoToEdit']").simulate("change", { target: { value: "test2changed" } });
+    expect(store.getState().todos.todoList.length).toEqual(2);
+    expect(store.getState().todos.todoList[1].description).toEqual("test2changed");
+
+    wrapper.find("button[name='delete']").last().simulate("click");
+    expect(store.getState().todos.todoList.length).toEqual(1);
+    expect(store.getState().todos.todoList[0].description).toEqual("test1");
   });
 });
